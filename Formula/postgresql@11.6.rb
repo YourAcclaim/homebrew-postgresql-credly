ca3a472159f5aab422f7f92258c29073937c5f67
B. Ruschill <brandon.ruschill@gmail.com>
2020-07-14 17:52:14 -0500

    add initial recipe

---
+++ Formula/postgresql@11.6.rb
@@ -0,0 +1,125 @@
+class PostgresqlAT116 < Formula
+  desc "Object-relational database system"
+  homepage "https://github.com/bruschill/homebrew-postgresql-credly"
+  version "11.6"
+  url "https://ftp.postgresql.org/pub/source/v11.6/postgresql-11.6.tar.bz2"
+  sha256 "49924f7ff92965fdb20c86e0696f2dc9f8553e1563124ead7beedf8910c13170"
+  revision 3
+
+  # bottle do
+  #   sha256 "420e0588a9c7d13bd60dbd14cd825e3ab94e8190aceae83a326469c2069eccff" => :catalina
+  #   sha256 "0f53a997e555a3dd79d58494b0b5d5e11d0d01bb20db2084d544cc750125ab2b" => :mojave
+  #   sha256 "2e37db63328194a9445083afbbd2e6ef22535bd6b15ba4514ee231b75e5a74f0" => :high_sierra
+  # end
+
+  # keg_only :versioned_formula
+
+  depends_on "pkg-config" => :build
+  depends_on "icu4c"
+  depends_on "openssl@1.1"
+  depends_on "readline"
+
+  uses_from_macos "libxml2"
+  uses_from_macos "libxslt"
+  uses_from_macos "perl"
+
+  on_linux do
+    depends_on "util-linux"
+  end
+
+  def install
+    ENV.prepend "LDFLAGS", "-L#{Formula["openssl@1.1"].opt_lib} -L#{Formula["readline"].opt_lib}"
+    ENV.prepend "CPPFLAGS", "-I#{Formula["openssl@1.1"].opt_include} -I#{Formula["readline"].opt_include}"
+
+    args = %W[
+      --disable-debug
+      --prefix=#{prefix}
+      --datadir=#{opt_pkgshare}
+      --libdir=#{opt_lib}
+      --includedir=#{opt_include}
+      --sysconfdir=#{etc}
+      --docdir=#{doc}
+      --enable-thread-safety
+      --with-bonjour
+      --with-gssapi
+      --with-icu
+      --with-ldap
+      --with-libxml
+      --with-libxslt
+      --with-openssl
+      --with-pam
+      --with-perl
+      --with-tcl
+      --with-uuid=e2fs
+    ]
+
+    # PostgreSQL by default uses xcodebuild internally to determine this,
+    # which does not work on CLT-only installs.
+    args << "PG_SYSROOT=#{MacOS.sdk_path}" if MacOS.sdk_root_needed?
+
+    system "./configure", *args
+    system "make"
+    system "make", "install-world", "datadir=#{pkgshare}",
+           "libdir=#{lib}",
+           "pkglibdir=#{lib}",
+           "includedir=#{include}",
+           "pkgincludedir=#{include}",
+           "includedir_server=#{include}/server",
+           "includedir_internal=#{include}/internal"
+  end
+
+  def post_install
+    return if ENV["CI"]
+
+    (var/"log").mkpath
+    (var/name).mkpath
+    unless File.exist? "#{var}/#{name}/PG_VERSION"
+      system "#{bin}/initdb", "--locale=C", "-E", "UTF-8", "#{var}/#{name}"
+    end
+  end
+
+  def caveats
+    <<~EOS
+      To migrate existing data from a previous major version of PostgreSQL run:
+        brew postgresql-upgrade-database
+    EOS
+  end
+
+  plist_options :manual => "pg_ctl -D #{HOMEBREW_PREFIX}/var/postgresql@credly start"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <true/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/postgres</string>
+          <string>-D</string>
+          <string>#{var}/#{name}</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/#{name}.log</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/#{name}.log</string>
+      </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    system "#{bin}/initdb", testpath/"test" unless ENV["CI"]
+    assert_equal opt_pkgshare.to_s, shell_output("#{bin}/pg_config --sharedir").chomp
+    assert_equal opt_lib.to_s, shell_output("#{bin}/pg_config --libdir").chomp
+    assert_equal opt_lib.to_s, shell_output("#{bin}/pg_config --pkglibdir").chomp
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# homebrew-production-postgres-version
+# homebrew-postgresql-credly
 Homebrew recipe for whatever postgresql version Acclaim (by Credly) production is on.
+
+### Installation Instructions:
+- tap the repo
+    - `brew tap bruschill/homebrew-postgresql-credly git@github.com:bruschill/homebrew-postgresql-credly.git`
+- install the recipe
+    - `brew install bruschill/postgresql-credly/postgresql@11.6`
+- stop your running installation of postgres
+    - if using homebrew, it's probably something like `brew services stop postgresql@11`
+- register and start the tapped version
+    - `brew services start bruschill/postgresql-credly/postgresql@11.6`
